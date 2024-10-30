fd10fd2b501d7c35c522abe8503803b40b391ea6
Manu <lawleagle@gmail.com>
2024-10-30 07:42:56 +0200

    reballanced moa speed and jumps

--- src/main/java/com/gildedgames/the_aether/entities/util/AetherMoaTypes.java
+++ src/main/java/com/gildedgames/the_aether/entities/util/AetherMoaTypes.java
@@ -11,10 +11,10 @@ public class AetherMoaTypes {
 	public static AetherMoaType blue, orange, white, black;
 
 	public static void initialization() {
-		blue = register("blue", 0x7777FF, new MoaProperties(3, 0.3F));
-		orange = register("orange", -0xC3D78, new MoaProperties(2, 0.6F));
-		white = register("white", 0xFFFFFF, new MoaProperties(4, 0.3F));
-		black = register("black", 0x222222, new MoaProperties(8, 0.3F));
+		blue = register("blue", 0x7777FF, new MoaProperties(1, 0.4F));
+		orange = register("orange", -0xC3D78, new MoaProperties(1, 0.6F));
+		white = register("white", 0xFFFFFF, new MoaProperties(2, 0.4F));
+		black = register("black", 0x222222, new MoaProperties(3, 0.4F));
 	}
 
 	public static AetherMoaType register(String name, int hexColor, MoaProperties properties) {
